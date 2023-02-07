10d8c549631f2f62756ca80892bd4ee7f7a216aa
tuniet <tofeju@gmail.com>
2023-02-07 18:30:21 +0000

    tabla y endpoints de artistas

--- src/api/models.py
+++ src/api/models.py
@@ -14,6 +14,25 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "email": self.email
+        }
+
+class Artist(db.Model):
+    ArtistId = db.Column(db.Integer, primary_key=True, nullable=False)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    description = db.Column(db.String(250), unique=False, nullable=False)
+    imageUrl = db.Column(db.String(120), unique=False, nullable=False)
+    musicUrl = db.Column(db.String(120), unique=False, nullable=False)
+
+
+    def __repr__(self):
+        return f'<Artist {self.id}>'
+
+    def serialize(self):
+        return {
+            "ArtistId": self.ArtistId,
+            "name": self.name,
+            "description": self.description,
+            "imageUrl" : self.imageUrl,
+            "musicUrl" : self.musicUrl
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -2,17 +2,40 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
-from api.models import db, User
+from api.models import db, User, Artist
 from api.utils import generate_sitemap, APIException
 
 api = Blueprint('api', __name__)
 
-
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
-
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+@api.route("/artist", methods = ["GET"])
+def getAllArtist():
+    artists = Artist.query.all()
+    response = [artist.serialize() for artist in artists]
+    response_body = {"message": "ok",
+                     "results": response
     }
-
     return jsonify(response_body), 200
+
+@api.route("/artist/<int:id>")
+def getArtistById(id):
+    artist = db.get_or_404(Artist, id)
+    response = artist.serialize()
+    response_body = {"message": "ok",
+                     "results": response
+    }
+    return jsonify(response_body), 200
+
+@api.route('/admin/artist/<int:artist_id>', methods=['PUT'])
+# @jwt_required()
+def update_news(artist_id):
+    request_body = request.get_json()
+    artist = Artist.query.get(artist_id)
+    if artist is None:
+        raise APIException('Artist not found', status_code=404)
+    if "description" and "name" and "imageurl" and "musicUrl" in request_body:
+        artist.description = request_body["description"]
+        artist.name = request_body["name"]
+        artist.imageUrl = request_body["imageUrl"]
+        artist.musicUrl = request_body["musicUrl"]
+    db.session.commit()
+    return jsonify(request_body), 200
