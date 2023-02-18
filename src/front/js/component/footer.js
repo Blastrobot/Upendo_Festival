ef2b888ff78bfb5074f85ce5af56f73adf1174ee
tuniet <tofeju@gmail.com>
2023-02-18 11:12:52 +0000

    final footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -10,10 +10,7 @@ export const Footer = () => (
       <div className="rrss col-6 d-flex justify-content-end" width="50%">
         <a className="bi bi-instagram h4 my-auto mx-2 text-white" href=""></a>
         <a className="bi bi-twitter h4 my-auto mx-2  text-white" href=""></a>
-        <a
-          className="bi bi-facebook h4 my-auto ms-2 me-4  text-white"
-          href=""
-        ></a>
+        <a className="bi bi-facebook h4 my-auto ms-2 me-4  text-white" href=""></a>
       </div>
     </div>
     <hr className="mt-0" />
@@ -24,13 +21,10 @@ export const Footer = () => (
           <input
             type="text"
             className="form-control"
-            placeholder="Enter your Email"
-          ></input>
+            placeholder="Enter your Email">
+          </input>
           <div className="input-group-append">
-            <button
-              className="btn btn-light btn-outline-dark text-dark ms-1"
-              type="button"
-            >
+            <button className="btn btn-light btn-outline-dark text-dark ms-1" type="button">
               Submit
             </button>
           </div>
@@ -38,7 +32,7 @@ export const Footer = () => (
       </div>
       <div className="contact col-6">
         <h5 className="text-center">CONTACT US</h5>
-        <ul className="text-center">
+        <ul className="text-center ps-0">
           <p>Contact Email: contact@upendofest.com</p>
           <p>Contact Tel.: 691383648</p>
           <p>Contact Adress: Cl. Edison, 3, 28006 Madrid</p>
@@ -47,42 +41,24 @@ export const Footer = () => (
     </div>
     <hr className="mt-0" />
     <div className="text-center d-flex justify-content-center">
-      <a
-        className="mx-2 text-white border-0"
-        style={{ textDecoration: "none" }}
-        href=""
-      >
+      <a className="mx-2 text-white border-0" style={{ textDecoration: "none" }} href="">
         Terms & conditions
       </a>
       <p> - </p>
-      <a
-        className="mx-2 text-white border-0"
-        style={{ textDecoration: "none" }}
-        href=""
-      >
+      <a className="mx-2 text-white border-0" style={{ textDecoration: "none" }} href="">
         Terms of use
       </a>
       <p> - </p>
-      <a
-        className="mx-2 text-white border-0"
-        style={{ textDecoration: "none" }}
-        href=""
-      >
+      <a className="mx-2 text-white border-0" style={{ textDecoration: "none" }} href="">
         Privacy policy
       </a>
       <p> - </p>
-      <a
-        className="mx-2 text-white border-0"
-        style={{ textDecoration: "none" }}
-        href=""
-      >
+      <a className="mx-2 text-white border-0" style={{ textDecoration: "none" }} href="">
         Cookie policy
       </a>
     </div>
     <div className="text-center pb-1">
-      <p>
-        <i className="bi bi-c-circle"></i> All Rigths Reserved Upendo Festival
-      </p>
+      <p><i className="bi bi-c-circle"></i> All Rigths Reserved Upendo Festival</p>
     </div>
   </footer>
 );
