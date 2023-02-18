1bcd9250e9bca3c36ecc71cc86a2e28e9a017acc
Charlie <carlosjcv94@gmail.com>
2023-02-18 09:26:02 +0000

    navbar logic full done

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -4,10 +4,12 @@ import ScrollToTop from "./component/scrollToTop";
 
 import { Home } from "./pages/home";
 import { Signup } from "./pages/signup";
+import { Admin } from "./pages/admin";
 import { Single } from "./pages/single";
+import { Tickets } from "./pages/tickets";
 import injectContext from "./store/appContext";
 
-import { Navbar } from "./component/navbar";
+import { Navbar } from "./component/navbar.jsx";
 import { Footer } from "./component/footer";
 
 //create your first component
@@ -20,9 +22,12 @@ const Layout = () => {
     <div>
       <BrowserRouter basename={basename}>
         <ScrollToTop>
+          <Navbar/>
           <Routes>
             <Route element={<Home />} path="/" />
             <Route element={<Signup />} path="/signup" />
+            <Route element={<Admin />} path="/admin" />
+            <Route element={<Tickets />} path="/tickets" />
             <Route element={<Single />} path="/single/:theid" />
             <Route element={<h1>Not found!</h1>} />
           </Routes>
---
+++ src/front/js/pages/tickets.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Tickets = () => {
+    return (
+        <div className="text-center mt-5">
+            <h1>Tickets View</h1>
+        </div>
+    );
+}
