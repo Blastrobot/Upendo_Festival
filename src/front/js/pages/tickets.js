c6e6fc7d2af9602f690f8fba5d090f5367975fc4
Charlie <carlosjcv94@gmail.com>
2023-08-05 23:06:20 +0000

    Node updated

--- src/front/js/pages/tickets.js
+++ src/front/js/pages/tickets.js
@@ -1,24 +1,3 @@
-// import React, { useContext } from "react";
-// import { Context } from "../store/appContext";
-// import { ProductCard } from "../component/productCard.jsx";
-
-// export const Tickets = () => {
-//     const { store, actions } = useContext(Context);
-
-//     return (
-//         <div className="container text-center mt-5">
-//             <h1>Tickets View</h1>
-//             <div className="row">
-//                 {store.productsArray.map((product, idx) => (
-//                     <div className="col-12" key={idx}>
-//                         <ProductCard product={product}/>
-//                     </div>
-//                 ))}
-//             </div>
-//         </div>
-//     );
-// }
-
 import React, { useContext } from "react";
 import { CartContext } from "../store/cartContext";
 import { productsArray } from "../store/productsStore";
